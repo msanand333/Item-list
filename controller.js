23d496936fccb2ae15d26dbccd77a32d974befa3
MS Anand <iamanand333@gmail.com>
2018-08-28 00:35:18 +0530

    controller modified

--- controller.js
+++ controller.js
@@ -1,13 +1,17 @@
-var app=angular.module("myList",[]);
-app.controller("myListController",function($scope){
-    $scope.items=["AngularJS","NodeJS","ReactJS"];
-    $scope.iName="";
-    $scope.add=function(iName){
-        $scope.items.push(iName);
-        $scope.iName="";
+var app = angular.module("myList", []);
+app.controller("myListController", function ($scope) {
+    $scope.items = ["AngularJS", "NodeJS", "ReactJS"];
+     $scope.iName="";
+
+    $scope.add = function (iName){
+        if ($scope.iName != "") {
+            $scope.items.push(iName);
+            $scope.iName = "";
+        }
     };
-    $scope.remove=function(index){
-        $scope.items.splice(index,1);
+
+    $scope.remove = function (index) {
+        $scope.items.splice(index, 1);
     };
 
 });
